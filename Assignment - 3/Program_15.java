94f5562fb9716cbd1726ce93ef0ccfd9ba40bda6
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-02-21 14:20:11 +0530

    Problems_onPattern

---
+++ Assignment - 3/Program_15.java
@@ -0,0 +1,82 @@
+/* Accept number of rows and number of columns from user and display below 
+pattern. 
+Input : iRow = 4 iCol = 4 
+Output : 
+		 1 2 3 4 
+		 1 * * 4 
+		 1 * * 4 
+		 1 2 3 4
+*/
+
+//----------------------------------------------------------
+// Imports
+//----------------------------------------------------------
+import java.util.Scanner;
+
+//----------------------------------------------------------
+// Function Name: DisplayPattern
+// Input        : Integer
+// Output       : Integer
+// Discription  : This function accept number of rows and number
+//                of columns from user and display pattern.   
+// Author       : Sneha Yogesh Choudhari
+//-----------------------------------------------------------
+
+class Pattern
+{
+	public
+	
+	void DisplayPattern(int iRow , int iCol) 
+	{ 
+		int iCnt1 , iCnt2 = 0;
+		
+		if(iRow != iCol)
+		{
+			return;
+		}
+		
+		for(iCnt1 = 1 ; iCnt1 <= iRow; iCnt1++)
+		{
+			for(iCnt2 = 1; iCnt2 <= iCol; iCnt2++)
+			{
+				if((iCnt1 == 1) || (iCnt2 == 1) || (iCnt2 == iCol) || (iCnt1 == iRow)) 
+				{
+					System.out.print(iCnt2 + " ");
+				}
+				else
+				{
+					System.out.print("*" + " ");
+				}
+			}
+			System.out.println();
+		}
+	} 
+}
+
+
+//----------------------------------------------------------
+// Entry point function
+//----------------------------------------------------------
+class Program_15
+{
+	public static void main(String b[])
+	{
+		int iValue1, iValue2 = 0;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter number of rows: ");
+		iValue1 = in.nextInt();
+		
+		System.out.print("Enter number of columns: ");
+		iValue2 = in.nextInt();
+		
+		Pattern obj = new Pattern();
+		
+		obj.DisplayPattern(iValue1,iValue2);
+	
+	}
+	
+}
+
+
---
+++ Assignment - 3/Program_21.java
@@ -0,0 +1,83 @@
+/* Accept number of rows and number of columns from user and display below 
+pattern. 
+Input : iRow = 4 iCol = 4 
+Output : 
+		 * # # # 
+		 * * # # 
+		 * * * # 
+		 * * * *
+*/
+
+//----------------------------------------------------------
+// Imports
+//----------------------------------------------------------
+import java.util.Scanner;
+
+//----------------------------------------------------------
+// Function Name: DisplayPattern
+// Input        : Integer
+// Output       : Integer
+// Discription  : This function accept number of rows and number
+//                of columns from user and display pattern.   
+// Author       : Sneha Yogesh Choudhari
+//-----------------------------------------------------------
+
+class Pattern
+{
+	public
+	
+	void DisplayPattern(int iRow , int iCol) 
+	{ 
+		int iCnt1 , iCnt2 = 0;
+		
+		if(iRow != iCol)
+		{
+			return;
+		}
+		
+		for(iCnt1 = 1; iCnt1 <= iRow; iCnt1++)
+		{
+			for(iCnt2 = 1; iCnt2 <= iCol; iCnt2++)
+			{
+				if(iCnt1 == iCnt2 || iCnt1 > iCnt2) 
+				{
+					System.out.print("*" + " ");
+				}
+				else
+				{
+					System.out.print("#" + " ");
+				}
+				
+			}
+			System.out.println();
+		}
+	} 
+}
+
+
+//----------------------------------------------------------
+// Entry point function
+//----------------------------------------------------------
+class Program_21
+{
+	public static void main(String b[])
+	{
+		int iValue1, iValue2 = 0;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter number of rows: ");
+		iValue1 = in.nextInt();
+		
+		System.out.print("Enter number of columns: ");
+		iValue2 = in.nextInt();
+		
+		Pattern obj = new Pattern();
+		
+		obj.DisplayPattern(iValue1,iValue2);
+	
+	}
+	
+}
+
+
